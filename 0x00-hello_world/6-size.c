ddf423ed9be8917c66dd5cf631e19491d83b9e55
F0rjb <ahmednasrigeo@gmail.com>
2023-07-16 13:06:57 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - A program that prints the size of various types of the comp it runs
+ * main - A program that prints the size of various comp types
  * Return 0 (Success)
  */
 int main(void)
