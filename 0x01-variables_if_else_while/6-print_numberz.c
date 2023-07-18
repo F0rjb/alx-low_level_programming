54202a3b3fdebfcce8908c7fcee7c4e513ccad76
F0rjb <ahmednasrigeo@gmail.com>
2023-07-17 19:15:17 -0700

    c is amazing \n

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints out all numbers under
+ * without using any char variables
+ * Return: 0
+ */
+int main(void)
+{
+int i = 48;
+
+while (i < 58)
+{
+putchar(i);
+i++;
+}
+putchar('\n');
+return (0);
+}
